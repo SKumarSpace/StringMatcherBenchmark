8a8ff35f8db2c58b633e8b33f717c72485547bbd
Shrey Kumar <skumar@callisto.enterprises>
2022-10-09 00:25:10 -0400

    Add project files.

---
+++ StringMatcherBenchmark/MyBenchmarks.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringMatcherBenchmark
+{
+    public class MyBenchmarks
+    {
+        //private const string inputString = "1G1ZG57B&9";
+        private const string outputString = "1G1ZG57B229";
+
+        [Benchmark]
+        public bool MatchReg()
+        {
+            return Regex.IsMatch(outputString, @"1G1ZG57B.9");
+        }
+
+        [Benchmark]
+        public bool MatchFile()
+        {
+            return FileSystemName.MatchesSimpleExpression("1G1ZG57B?9", outputString);
+        }
+    }
+}
---
+++ StringMatcherBenchmark/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using StringMatcherBenchmark;
+
+Console.WriteLine("Hello, World!");
+
+//var x = new MyBenchmarks();
+//Console.WriteLine(x.MatchReg());
+//Console.WriteLine(x.MatchFile());
+
+var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
